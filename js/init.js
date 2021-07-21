828d30163862bc4555323fb65e5e50f49520c489
AdrianMM1 <87627537+AdrianMM1@users.noreply.github.com>
2021-07-20 22:55:49 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAcVklsEscpXNGKYupFHIgh16mld5_6aEw",
+    authDomain: "chat801amm.firebaseapp.com",
+    projectId: "chat801amm",
+    storageBucket: "chat801amm.appspot.com",
+    messagingSenderId: "606339904841",
+    appId: "1:606339904841:web:047a08269caf2640d91698",
+    measurementId: "G-PDP1D94HXC"
+    });
